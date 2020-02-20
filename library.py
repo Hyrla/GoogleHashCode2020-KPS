65f0c67e8f3e7ffa4c2db4b6287aa7774f0bd604
manonfrnr <fournier.manon@outlook.com>
2020-02-20 19:35:51 +0100

    [ADD] Class Library[ADD] Class Library[ADD] Class Library[ADD] Class Library[ADD] Class Library

--- library.py
+++ library.py
@@ -1,2 +1,5 @@
-class library:
-    list books = []
+class Library:
+    def __init__(self, books, nb_days, nb_scan_by_days):
+        self.books = books
+        self.nb_day = nb_days
+        self.nb_scan_by_days = nb_scan_by_days
