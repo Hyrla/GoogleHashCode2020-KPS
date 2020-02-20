1c05db1e82e110ed7d4e9f592aa624299f3952b0
manonfrnr <fournier.manon@outlook.com>
2020-02-20 21:40:10 +0100

    [ADD] Fichier write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import read_file
 import scoring
+import write_file
 
 books = []
 libraries = []
@@ -7,4 +8,7 @@ libraries = []
 nbDays = read_file.reading_file("a_example.txt", books, libraries)
 print(books)
 print(libraries)
-print(scoring.scoreAllLibs(libraries,nbDays))
+#librarie = libraries[:]
+libToScan = scoring.scoreAllLibs(libraries,nbDays)
+#print(libToScan)
+write_file.write_file(libToScan)
